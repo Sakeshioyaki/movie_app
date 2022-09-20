17e9eb0f289f3f9931b5d87f8f8ac4d13e7a92c8
Nguyệt Ánh <55714975+Sakeshioyaki@users.noreply.github.com>
2022-09-20 16:21:25 +0700

    udate dio dioo

--- lib/networks/api_client.dart
+++ lib/networks/api_client.dart
@@ -3,9 +3,9 @@ import 'package:movie_app/models/trending_movie/trending_movie.dart';
 import 'package:movie_app/models/up_coming/up_coming_movie.dart';
 import 'package:retrofit/retrofit.dart';
 
-part 'client.g.dart';
+part 'api_client.g.dart';
 
-@RestApi()
+@RestApi(baseUrl: "https://api.openweathermap.org")
 abstract class ApiClient {
   factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
@@ -17,10 +17,9 @@ abstract class ApiClient {
 
   @GET("/trending/movie/{time_window}")
   Future<UpComingMovie> getUpComingMovie(
-    @Path("id") String id,
     @Query('api_key') String apiKey, {
-    @Query('language') String language,
-    @Query('page') int page,
-    @Query('region') String region,
+    @Query('language') String? language,
+    @Query('page') int? page,
+    @Query('region') String? region,
   });
 }
